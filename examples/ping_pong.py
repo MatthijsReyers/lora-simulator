cdb6a1ce38216fc7ed164256018d10d691c3c360
matthijsreyers <matthijsreyers@gmail.com>
2026-05-20 09:54:16 +0200

    Cleaned up ping pong example

--- examples/ping_pong.py
+++ examples/ping_pong.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
-import sys, time, asyncio, logging
-import logging
+import sys
 
 sys.path.append('.')
 
+from simulator.lora.packet import LoraPacket
 from simulator.lora.phy_layer import LoraPhyLayer
 from simulator.lora.radio import LoraRadio
 from simulator.environment import simulation_env as sim
-import random
+import random, asyncio, logging
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 class Node:
-    def __init__(self, code_rate):
+    def __init__(self, code_rate: int):
         self.radio = LoraRadio()
         self.code_rate = code_rate
         sim.create_task(self.recv())
@@ -44,30 +44,30 @@ class Node:
 
 if __name__ == "__main__":
 
-    level = logging.INFO
+    # level = logging.INFO
     
-    ch = logging.StreamHandler()
-    ch.setLevel(level)
-    formatter = logging.Formatter("%(levelname)s;%(message)s")
-    ch.setFormatter(formatter)
+    # ch = logging.StreamHandler()
+    # ch.setLevel(level)
+    # formatter = logging.Formatter("%(levelname)s;%(message)s")
+    # ch.setFormatter(formatter)
 
-    logger.addHandler(ch)
+    # logger.addHandler(ch)
 
-    sim.logger.setLevel(logging.INFO)
-    sim.logger.addHandler(ch)
+    # sim.logger.setLevel(logging.INFO)
+    # sim.logger.addHandler(ch)
 
-    phy_layer = LoraPhyLayer()
-    phy_layer.logger.setLevel(level)
-    phy_layer.logger.addHandler(ch)
+    # phy_layer = LoraPhyLayer()
+    # phy_layer.logger.setLevel(level)
+    # phy_layer.logger.addHandler(ch)
 
     node1 = Node(code_rate=7)
     node2 = Node(code_rate=7)
 
-    node1.radio.logger.setLevel(level)
-    node1.radio.logger.addHandler(ch)
+    # node1.radio.logger.setLevel(level)
+    # node1.radio.logger.addHandler(ch)
     
-    node2.radio.logger.setLevel(level)
-    node2.radio.logger.addHandler(ch)
+    # node2.radio.logger.setLevel(level)
+    # node2.radio.logger.addHandler(ch)
 
     sim.run(simulation_length=120)
 
