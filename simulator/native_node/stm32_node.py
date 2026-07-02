99944594bfa6de993d9651bd2532b7dd89d73df0
matthijsreyers <matthijscrafter@gmail.com>
2026-07-02 15:33:17 +0200

    Added straight copy of stm32wl radio driver

--- simulator/native_node/stm32_node.py
+++ simulator/native_node/stm32_node.py
@@ -1,15 +1,23 @@
 from typing import Tuple, override
 from os import path
 from simulator.lora.radio_power_profile import RadioPowerProfile, Stm32wl55PowerProfile
-from .radio_node import RadioNode
-from .stm32_enums import HAL_StatusTypeDef
+from simulator.native_node.radio_node import RadioNode
+from simulator.native_node.stm32wl.hal_enums import HAL_StatusTypeDef
+import glob
 
 class STM32Node(RadioNode):
     """
     A simulated node running native STM32 code via CFFI.
     """
-    _STM32_HAL_SOURCE = path.join(path.dirname(path.realpath(__file__)), 'stm32_hal.c')
-    _STM32_HAL_SHIM_DIR = path.join(path.dirname(path.realpath(__file__)), 'stm32_hal_shim')
+    _STM32_HAL_SHIM_DIR = path.join(path.dirname(path.realpath(__file__)), 'stm32wl/hal_headers')
+    _STM32_HAL_STUBS = path.join(path.dirname(path.realpath(__file__)), 'stm32wl/hal_stubs')
+    _STM32_HAL_SOURCES = [ f for f in glob.glob(f'{_STM32_HAL_STUBS}/*.c') ]
+    
+
+    _STM32_RADIO_SHIM_DIR = path.join(path.dirname(path.realpath(__file__)), 'stm32wl/radio_driver_headers')
+    _STM32_RADIO_STUBS = path.join(path.dirname(path.realpath(__file__)), 'stm32wl/radio_driver_stubs')
+    _STM32_RADIO_SOURCES = [ f for f in glob.glob(f'{_STM32_RADIO_STUBS}/*.c') ]
+
 
     def __init__(
             self, 
@@ -21,10 +29,11 @@ class STM32Node(RadioNode):
             ffi_backend = None,
             boot_delay: float = 0.0,
         ):
-        hal_files = [self._STM32_HAL_SOURCE]
+        hal_files = self._STM32_HAL_SOURCES
+        hal_files.extend(self._STM32_HAL_SOURCES)
         if extra_source_files:
             hal_files.extend(extra_source_files)
-        hal_includes = [self._STM32_HAL_SHIM_DIR]
+        hal_includes = [self._STM32_HAL_SHIM_DIR, self._STM32_RADIO_SHIM_DIR]
         if include_dirs:
             hal_includes.extend(include_dirs)
         super().__init__(
@@ -41,9 +50,3 @@ class STM32Node(RadioNode):
     @override
     def setup_callbacks(self):
         super().setup_callbacks()
-
-        @self.export('int(void*)')
-        def HAL_SUBGHZ_Init(_ptr: int) -> int:
-            self.radio_initialized = True
-            return HAL_StatusTypeDef.HAL_OK
-        
